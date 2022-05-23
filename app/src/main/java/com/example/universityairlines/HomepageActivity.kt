65378b9d6f2a8bcba475c95c5fbe54ee447b2a8b
alebullegas <alebullegas31@gmail.com>
2022-05-23 16:03:38 +0200

    homepage added

---
+++ app/src/main/java/com/example/universityairlines/HomepageActivity.kt
@@ -0,0 +1,13 @@
+package com.example.universityairlines
+
+//import android.support.v7.app.AppCompatActivity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HomepageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_homepage)
+    }
+}
