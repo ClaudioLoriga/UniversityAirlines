f3ea39550eb18e5a55573170702e3bc52fd1416b
ClaudioLoriga <odiglodi@gmail.com>
2022-05-24 17:48:14 +0200

    Added retrofit + progress LoginActivity

--- app/src/main/java/com/example/universityairlines/login/LoginActivity.kt
+++ app/src/main/java/com/example/universityairlines/login/LoginActivity.kt
@@ -1,16 +1,47 @@
 package com.example.universityairlines.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import com.example.universityairlines.HomepageActivity
+import com.example.universityairlines.OkHttpRequest
 import com.example.universityairlines.R
+import com.example.universityairlines.databinding.ActivityMainBinding
+import com.example.universityairlines.databinding.LoginLayoutBinding
+import com.example.universityairlines.databinding.SplashLayoutBinding
+import retrofit2.Retrofit
+import retrofit2.converter.jackson.JacksonConverterFactory
 
 class LoginActivity : AppCompatActivity() {
 
-    @Override
+    private lateinit var binding: LoginLayoutBinding
+    private var retrofit: Retrofit =
+        Retrofit.Builder().addConverterFactory(JacksonConverterFactory.create()).build()
+    private var okHttpRequest: OkHttpRequest = OkHttpRequest(retrofit)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login_layout)
 
+        binding = LoginLayoutBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.loginbutton.setOnClickListener { }
 
     }
+
+    fun checkUser() {
+        okHttpRequest.retroGET(binding.edittextemail.text.toString(), binding.edittextpassword.text.toString()) { loginResponse ->
+            if (loginResponse.isSuccessful && loginResponse.body()?.status == "200") {
+                val intent: Intent = Intent(this, HomepageActivity::class.java)
+                startActivity(intent)
+            } else {
+
+            }
+
+        }
+    }
+
+
 }
